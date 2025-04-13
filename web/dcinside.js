2d337edda449bc7d12da99c783a0bfebf50217c2
dadavuvu <dagonunsu@naver.com>
2025-04-14 05:11:40 +0900

    Image Bug Fix & Link Logic Changed & Page Counter Added

--- web/dcinside.js
+++ web/dcinside.js
@@ -1,46 +1,48 @@
 document.addEventListener('DOMContentLoaded', async function () {
-    const urlParams = new URL(window.location.href).searchParams;
-    if (!urlParams.has('id') || !urlParams.has('no')) return document.write("MISSING_PARAMETERS");
-    let data = await fetch(`http://127.0.0.1:5050/proxy/dc/mgallery/board/view/?id=${urlParams.get("id")}&no=${urlParams.get("no")}`)
-    const parser = new DOMParser();
-    data = parser.parseFromString(await data.text(), 'text/html')
+  const urlParams = new URL(window.location.href).searchParams;
+  if (!urlParams.has('id') || !urlParams.has('no')) return document.write("MISSING_PARAMETERS");
+  let data = await fetch(`http://127.0.0.1:5050/proxy/dc/mgallery/board/view/?id=${urlParams.get("id")}&no=${urlParams.get("no")}`)
+  const parser = new DOMParser();
+  data = parser.parseFromString(await data.text(), 'text/html')
 
-    if (data.querySelector(".delet")) return document.write("UNKNOWN_GALLERY");
-    if (data.head.innerHTML.indexOf('alert("해당 갤러리는 존재하지 않습니다.");') != -1) return document.write("UNKNOWN_ARTICLE");
+  if (data.querySelector(".delet")) return document.write("UNKNOWN_GALLERY");
+  if (data.head.innerHTML.indexOf('alert("해당 갤러리는 존재하지 않습니다.");') != -1) return document.write("UNKNOWN_ARTICLE");
 
-    /**
-     * @type {HTMLElement}
-     */
-    const content = data.querySelector(".write_div")
+  const content = data.querySelector(".write_div")
 
-    for (const element of content.querySelectorAll("img")) {
-        element.style.cssText = ""
-        element.setAttribute("onclick", "")
-        element.setAttribute("onerror", "")
-        element.setAttribute("alt", "")
-        element.parentNode.innerHTML = `<div class="image-container">${element.parentNode.innerHTML}</div>`
+  for (const element of content.querySelectorAll("img")) {
+    element.style.cssText = ""
+    element.setAttribute("onclick", "")
+    element.setAttribute("onerror", "")
+    element.setAttribute("alt", "")
+    if (element.src == "https://nstatic.dcinside.com/dc/m/img/gallview_loading_ori.gif") {
+      element.src = element.getAttribute("data-original")
     }
+    element.parentNode.innerHTML = `<div class="image-container">${element.parentNode.innerHTML}</div>`
+    // 씨 발놈의 이미지
+    // element.remove()
+  }
 
-    for (const element of content.querySelectorAll("#spoiler_warning")) {
-        element.remove()
+  for (const element of content.querySelectorAll("#spoiler_warning")) {
+    element.remove()
+  }
+
+  for (const element of content.querySelectorAll("a")) {
+    if (element.href.indexOf("https://gall.dcinside.com/mgallery/board/view") != -1) {
+      element.setAttribute("target", "")
+      element.href = "/dcinside.html?" + new URL(element.href).searchParams.toString() + (urlParams.has('addbottom') && `&addbottom=${urlParams.get('addbottom')}`)
     }
+  }
 
-    for (const element of content.querySelectorAll("a")) {
-        if (element.href.indexOf("https://gall.dcinside.com/mgallery/board/view") != -1) {
-            element.setAttribute("target", "")
-            element.href = "/dcinside.html?" + new URL(element.href).searchParams.toString() + (urlParams.has('addbottom') && `&addbottom=${urlParams.get('addbottom')}`)
-        }
-    }
+  document.querySelector('.content').innerHTML = content.innerHTML
 
-    document.querySelector('.content').innerHTML = content.innerHTML
+  const images = [...document.querySelectorAll("img")];
 
-    const images = [...document.querySelectorAll("img")];
+  const proms = images.map(im => new Promise(res =>
+    im.onload = () => res([im.width, im.height])
+  ))
 
-    const proms = images.map(im => new Promise(res =>
-        im.onload = () => res([im.width, im.height])
-    ))
-
-    Promise.all(proms).then(data => {
-        Loaded()
-    })
+  Promise.all(proms).then(data => {
+    Loaded()
+  })
 });
--- web/scripts.js
+++ web/scripts.js
@@ -1,103 +1,115 @@
 document.addEventListener('DOMContentLoaded', async function () {
-    function setTheme() {
-        document.body.className = ""
-        document.body.classList.add(window.localStorage.getItem("theme"))
-    }
+  function setTheme() {
+    document.body.className = ""
+    document.body.classList.add(window.localStorage.getItem("theme"))
+  }
 
-    document.querySelector('#setting').addEventListener('click', function () {
-        window.localStorage.setItem("theme", (window.localStorage.getItem("theme") == "white" && "black") || (window.localStorage.getItem("theme") == "black" && "backlight") || "white");
-        setTheme()
-    });
-
+  document.querySelector('#setting').addEventListener('click', function () {
+    window.localStorage.setItem("theme", (window.localStorage.getItem("theme") == "white" && "black") || (window.localStorage.getItem("theme") == "black" && "backlight") || "white");
     setTheme()
+  });
+
+  setTheme()
 });
 
 function Loaded() {
-    const content = document.querySelector('.content');
+  const content = document.querySelector('.content');
 
-    const urlParams = new URL(window.location.href).searchParams;
-    if (urlParams.has('addbottom')) {
-        document.querySelector('main').style.paddingBottom = 2+Number(urlParams.get("addbottom"))+"lh"
-    };
+  const urlParams = new URL(window.location.href).searchParams;
+  if (urlParams.has('addbottom')) {
+    document.querySelector('main').style.paddingBottom = 2 + Number(urlParams.get("addbottom")) + "lh"
+    document.querySelector('#page-indicator').style.bottom = .5 + Number(urlParams.get("addbottom")) + "lh"
+  };
 
-    // 이미지 컨테이너 크기 위치 조절
-    const vh = document.body.clientHeight
-    const lh = vh / 30
+  // 현재 페이지 인덱스
+  let currentPage = 0;
+  const lineHeight = Number(window.getComputedStyle(content).lineHeight.replace("px", ""))
 
-    document.querySelectorAll(".image-container").forEach(element => {
-        element.style.height = `${Math.ceil(element.querySelector("img").height / lh)}lh`
-        let startBasedLine = Math.floor(element.offsetTop / lh)-2
-        let startBasedPageLine = startBasedLine%26
-        let endBasedLine = startBasedLine + Math.ceil(element.querySelector("img").height / lh)
-        let endBasedPageLine = endBasedLine%26
+  document.querySelectorAll(".image-container").forEach(element => {
+    const img = element.querySelector("img");
+    const imgHeightLh = Math.ceil(img.height / lineHeight);
 
-        if (startBasedPageLine > endBasedPageLine) {
-            element.style.marginTop = startBasedPageLine+endBasedPageLine-26 + "lh"
-        }
-        else if (Math.ceil(element.querySelector("img").height / lh) >= 26) {
-            element.style.marginTop = 25-startBasedPageLine + "lh"
-        }
-        console.log(startBasedLine, endBasedLine, startBasedPageLine, endBasedPageLine, startBasedLine+Math.ceil(element.querySelector("img").height / lh))
+    element.style.height = `${imgHeightLh}lh`;
+    // const startLine = Math.floor((element.offsetTop - 2 * lineHeight) / lineHeight);
+
+    const containerTop = content.getBoundingClientRect().top;
+    const elementTop = element.getBoundingClientRect().top;
+  
+    const offset = elementTop - containerTop + content.scrollTop;
+    const globalLineIndex = Math.floor(offset / lineHeight);
+  
+    const pageLine = (globalLineIndex % 26);
+    const endPageLine = pageLine + imgHeightLh;
+    console.log(element, endPageLine, pageLine)
+
+    if (endPageLine > 26) {
+      element.style.marginTop = `${26 - pageLine}lh`;
+    }
+  });
+
+  const totalPages = Math.ceil(content.scrollHeight / content.clientHeight);
+
+  // 페이지 넘김 함수
+  function goToPage(pageIndex) {
+    if (pageIndex < 0 || pageIndex >= totalPages) return;
+    currentPage = pageIndex;
+    content.scrollTo({
+      top: currentPage * lineHeight * 26
     });
+  }
 
-    // 현재 페이지 인덱스
-    let currentPage = 0;
+  document.querySelector('#prev-web').addEventListener('click', function () {
+    history.back();
+  });
 
-    // 페이지 넘김 함수
-    function goToPage(pageIndex) {
-        const lineHeight = Number(window.getComputedStyle(content).lineHeight.replace("px", ""))
-        const totalPages = Math.ceil(content.scrollHeight / content.clientHeight);
-        if (pageIndex < 0 || pageIndex >= totalPages) return;
-        currentPage = pageIndex;
-        content.scrollTo({
-            top: currentPage * lineHeight * 30
-        });
+  document.querySelector('#next-web').addEventListener('click', function () {
+    history.forward();
+  });
+
+  document.querySelector('#go-root').addEventListener('click', function () {
+    location.href = "/"
+  });
+
+  document.querySelector(".loading").remove()
+
+  const toggleButton = document.querySelector("#toggleButton");
+  const navbar = document.querySelector("#navbar");
+  const closeNavbar = document.querySelector("#untoggleButton");
+  const main = document.querySelector("main");
+
+  toggleButton.addEventListener("click", () => {
+    navbar.classList.toggle("show");
+    closeNavbar.classList.toggle("unshow");
+    main.classList.toggle("navi");
+  });
+
+  closeNavbar.addEventListener("click", () => {
+    navbar.classList.remove("show");
+    closeNavbar.classList.add("unshow");
+    main.classList.remove("navi");
+  });
+
+  const pageIndicator = document.querySelector('#page-indicator');
+
+  let hideTimeout;
+
+  const showIndicator = (text) => {
+    pageIndicator.innerText = text;
+    pageIndicator.classList.add("show")
+    clearTimeout(hideTimeout);
+    hideTimeout = setTimeout(() => pageIndicator.classList.remove("show"), 1000);
+  };
+
+  document.body.addEventListener('click', (e) => {
+    let linkClicked = e.target.closest('a') || e.target.closest('button') || e.target.closest('#navbar');
+    if (linkClicked) return;
+
+    if (e.clientX > window.innerWidth / 2) {
+      goToPage(currentPage + 1);
+    } else {
+      goToPage(currentPage - 1);
     }
 
-    // 버튼 클릭 이벤트 핸들러
-    document.querySelector('#prev-page').addEventListener('click', function () {
-        goToPage(currentPage - 1);
-    });
-
-    document.querySelector('#next-page').addEventListener('click', function () {
-        goToPage(currentPage + 1);
-    });
-
-    document.querySelector('#prev-web').addEventListener('click', function () {
-        history.back();
-    });
-
-    document.querySelector('#next-web').addEventListener('click', function () {
-        history.forward();
-    });
-
-    document.querySelector('#go-root').addEventListener('click', function () {
-        location.href = "/"
-    });
-
-    let clickable = false
-    document.querySelector('#click').addEventListener('click', function () {
-        clickable = !clickable
-        document.querySelector('#next-page').style.display = clickable && "none" || ""
-        document.querySelector('#prev-page').style.display = clickable && "none" || ""
-    });
-
-    document.querySelector(".loading").remove()
-
-    const toggleButton = document.querySelector("#toggleButton");
-    const navbar = document.querySelector("#navbar");
-    const closeNavbar = document.querySelector("#untoggleButton");
-    const main = document.querySelector("main");
-
-    toggleButton.addEventListener("click", () => {
-        navbar.classList.toggle("show");
-        closeNavbar.classList.toggle("unshow");
-        main.classList.toggle("navi");
-    });
-
-    closeNavbar.addEventListener("click", () => {
-        navbar.classList.remove("show");
-        closeNavbar.classList.add("unshow");
-        main.classList.remove("navi");
-    });
+    showIndicator(currentPage + 1 + " / " + totalPages)
+  });
 }
