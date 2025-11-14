f9a21b9d9551939e94d60aa6fc623bdc03afd635
dadavuvu <dagonunsu@naver.com>
2025-11-15 01:00:20 +0900

    V0.81

--- index.js
+++ index.js
@@ -22,7 +22,10 @@ async function renderWithPuppeteer(url) {
     const executablePath = "/system/bin/chrome";
     const browser = await puppeteer.launch({
       headless: true,
-      executablePath
+      args: [
+        "--no-sandbox",
+        "--disable-gpu",
+      ]
     });
     const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/  58.0.3029.110 Safari/537.3');
